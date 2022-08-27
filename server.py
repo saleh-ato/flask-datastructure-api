414ab72dc6e37ec7b67e70265cfa4205d5dbd7a3
Saleh Toodarvari <saleh.toodarvari@gmail.com>
2022-08-27 16:34:21 +0430

    hash table compeleted!

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import linked_list
+import hash_table
 
 app=Flask(__name__)
 
@@ -114,7 +115,26 @@ def delete_user(user_id):
 
 @app.route("/blog_post/<user_id>", methods=["POST"])
 def create_blog_post(user_id):
-    pass
+    data = request.get_json()
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return jsonify({"message":"user does not exist!"}), 400
+    
+    ht = hash_table.HashTable(10)
+    ht.add_key_value("title", data["title"])
+    ht.add_key_value("body", data["body"])
+    ht.add_key_value("date", now)
+    ht.add_key_value("user_id", user_id)
+
+    new_blog_post = BlogPost(
+        title = ht.get_value("title"),
+        body = ht.get_value("body"),
+        date = ht.get_value("date"),
+        user_id = ht.get_value("user_id")
+    )
+    db.session.add(new_blog_post)
+    db.session.commit()
+    return jsonify({"message":"new blog post created!"}), 200
 
 @app.route("/user/<user_id>", methods=["GET"])
 def get_all_blog_posts(user_id):
