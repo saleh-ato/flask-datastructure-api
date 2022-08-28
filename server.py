cbc9dbee7802882ad88f0f49dfdb48aff50d221d
Saleh Toodarvari <saleh.toodarvari@gmail.com>
2022-08-28 22:25:25 +0430

    C:/Program Files/Git/blog_post/<blog_post_id> route modified

--- server.py
+++ server.py
@@ -7,6 +7,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import linked_list
 import hash_table
+import binary_search_tree
+import random
 
 app=Flask(__name__)
 
@@ -136,9 +138,25 @@ def create_blog_post(user_id):
     db.session.commit()
     return jsonify({"message":"new blog post created!"}), 200
 
-@app.route("/user/<user_id>", methods=["GET"])
-def get_all_blog_posts(user_id):
-    pass
+@app.route("/blog_post/<blog_post_id>", methods=["GET"])
+def get_all_blog_posts(blog_post_id):
+    blog_posts = BlogPost.query.all()
+    random.shuffle(blog_posts)
+    bst = binary_search_tree.BinarySearchTree()
+    for post in blog_posts:
+        bst.insert({
+            "id":post.id,
+            "title":post.title,
+            "body":post.body,
+            "user_id":post.user_id
+        })
+    
+    post = bst.search(blog_post_id)
+
+    if not post:
+        return jsonify({"message":"post not found!"})
+    return jsonify(post)
+
 
 @app.route("/blog_post/<blog_post_id>", methods=["GET"])
 def get_one_blog_post(blog_post_id):
