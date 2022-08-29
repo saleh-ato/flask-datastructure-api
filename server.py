198ca6eacd06cb99d01e87b0d853994c837c21b4
Saleh Toodarvari <saleh.toodarvari@gmail.com>
2022-08-29 14:32:22 +0430

    stack and route for delete last 10 blogpost implemented

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ import hash_table
 import binary_search_tree
 import random
 import custom_q
+import stack
 
 app=Flask(__name__)
 
@@ -184,9 +185,17 @@ def get_numeric_post_bodies():
         })
     return jsonify(return_list)
 
-@app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
-def delete_blog_post(blog_post_id):
-    pass
+@app.route("/blog_post/delete_last_10", methods=["DELETE"])
+def delete_last_10():
+    blog_posts = BlogPost.query.all()
+    s = stack.Stack()
+    for post in blog_posts:
+        s.push(post)
+    for _ in range(10):
+        post_to_delete = s.pop()
+        db.session.delete(post_to_delete.data)
+        db.session.commit()
+    return jsonify({"message":"success"})
 
 if __name__=="__main__":
     app.run(debug=True)
